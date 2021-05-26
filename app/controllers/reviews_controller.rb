5aa8a15637667697b3bbbbf5fb93796ebea52b0a
Chloe Avenas <avenaschloe@gmail.com>
2021-05-26 16:50:05 +0100

    complete restaurants yel MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # the above is looking in the URL for the restaurant instance which :restaurant_id is in the path
+    @review.restaurant = @restaurant
+    # raise
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning DB..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+pizza_east = { name: "Pizza East", address: "Shoreditch High St - London", category: "italian" }
+emilias = { name: "Emilia's Pasta", address: "St Katharine's Docks - London", category: "italian" }
+sushi_samba = { name: "Sushi Samba", address: "Salesforce Tower - London", category: "japanese" }
+duck_and_waffle = { name: "Duck and Waffle", address: "Bishopsgate - London", category: "belgian" }
+la_petite_auberge = { name: "La Petite Auberge", address: "Upper St - London", category: "french" }
+
+[ pizza_east, emilias, sushi_samba, duck_and_waffle, la_petite_auberge ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
